361f88b36ec61bc686bf841c486dd2e684502809
carol <carolinebaptista41@gmail.com>
2018-01-23 20:42:36 -0200

    inclusao do index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8"/>
+
+<meta name="viewport" content="width=device-width" />
+	<title>login RH</title>
+	
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="estilo/estilo.css" />
+  <link rel="icon" type="image/jpg" href="i.jpg">
+	
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+</head>
+<body class="container">
+
+
+<header>
+	
+</header>
+<p></p>
+<h2>Página RH</h2>
+<p></p>
+<br/>
+<h3 style="font-family: arial;">Faça o seu login</h3>
+<hr/>
+<form >
+  <div class="form-group ">
+    <label for="exampleInputEmail1">CPF</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" Digite o seu CPF">
+    <small id="emailHelp" class="form-text text-muted">Nós nunca compartilhamos as suas informações com ninguém.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Digite a sua senha">
+  </div>
+ Não tem uma conta?<a href=""> Cadastre-se!</a>
+ <p></p>
+  <button style="" type="submit" class="btn btn-dark btn-outline">Entrar</button>
+ 
+</form>
+
+</body>
+</html>
