6e4043b9e69cf3e94c6cfc4069784de92fa4328b
gabriel <gabrielcandidobrandao@gmail.com>
2018-01-23 19:47:23 -0200

    adicionando reajuste

---
+++ reajuste.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width">
+	 
+
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="estilo/estilo.css">
+	
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+
+	<script src="js/bootstrap.min.js"></script>
+	</head>
+
+
+
+<body>
+	<div class="container-fluid superior ">
+	
+	<div class="container">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+<h2>Acordo coletivo</h2>
+
+<h5>Valor do reajuste (%)</h5>
+	<form method="post" action="aula.php">
+		<input type="text" name="reajuste" placeholder="" />
+						
+		<button class="btn btn-outline-primary my-2 my-sm-0">Reajustar</button>
+
+	</form>
+
+
+</body>
+</html>
