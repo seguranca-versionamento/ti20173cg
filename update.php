47f54ff47360f9a205414531eed8fe97a144929d
gabriel <gabrielcandidobrandao@gmail.com>
2018-01-23 21:49:04 -0200

    update.php

---
+++ update.php
@@ -0,0 +1,40 @@
+<?php 
+include 'conexao.php';
+
+$reajuste = $_POST['reajuste'];
+
+
+$reajuste = ($reajuste/100)+1;
+
+$sql = "UPDATE funcionarios SET salario= $reajuste * salario";
+
+if ($conn->query($sql) === TRUE) {
+	echo "Reajuste atualizado com sucesso";
+
+	# code...
+}
+else{
+	echo "Erro: ". $conn-> error;
+}
+
+$conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
