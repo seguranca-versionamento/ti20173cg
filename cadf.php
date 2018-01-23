240d429c9aee970518aa1a63baaeadb67e7218f4
Presida <luizfeliperz@gmail.com>
2018-01-23 20:54:08 -0200

    Inclusão Cadastro de Funcionário

---
+++ cadf.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Bolsonaro Presidente!</title>
+   </head>
+  <body>
+   
+  
+    <div class="container">
+		<section>
+		<h1 class="bg-dark text-white">Cadastrar Funcionário</h1><br>
+		<form method="POST" action="#"> 
+
+		<div class="form-group">
+     		<label for="cpf">CPF: </label>
+			<input type="text" name="cpf" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="senha">Senha: </label>
+			<input type="password" name="senha" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="nome">Nome: </label>
+			<input type="text" name="nome" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="nascimento">Nascimento: </label>
+			<input type="date" name="nascimento" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="cargo">Cargo: </label>
+			<input type="text" name="cargo" class="form-control form-control-lg" />
+		</div>
+
+	         
+		<div class="form-group">
+			<label for="sexo">Sexo: </label>
+			<select name="sexo" name="sexo" class="form-control form-control-lg">
+				<option value="">Masculino</option>
+				<option value="">Feminino</option>
+			</select>
+		</div>
+
+
+		<div class="form-group">
+			<label for="salario">Salário: </label>
+			<input type="text" name="salario" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="contratacao">Contratação: </label>
+			<input type="date" name="contratacao" class="form-control form-control-lg" />
+		</div>
+
+
+		<div class="form-group">
+			<label for="telefone">Telefone: </label>
+			<input type="tel" name="telefone" class="form-control form-control-lg" />
+		</div>
+
+
+    	<div class="form-group">
+			<label for="endereco">Endereço: </label>
+			<input type="text" name="endereco" class="form-control form-control-lg" />
+		</div>
+
+		<button type="submit" class="btn btn-dark btn-lg">Enviar</button>
+			
+		</form>
+		</section>
+	</div>
+
+
+	 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
